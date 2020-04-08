1673138fb8ab74526bb65817f6e5258d6f33eec1
g4b0 <g4b0@g4b0s-MBP.fritz.box>
2020-04-08 21:58:22 +0200

    added cl option for nlast lines, input filename is now used for figure title

--- Pegelstaende/plot.py
+++ Pegelstaende/plot.py
@@ -13,9 +13,8 @@ Todo:
 
 import argparse
 import sys
+import os
 
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -40,6 +39,7 @@ def parseArguments():
                         type=str, default='plot',
                         help='Filename for plot')
   parser.add_argument('-t', '--threshold', type=float, help='Threshold is shown as horizontal line, format <x.x>')  
+  parser.add_argument('-n', dest='nlast', type=int, help='Plots only last n lines of infile')  
 
   return parser.parse_args()
 
@@ -47,8 +47,12 @@ if __name__ == '__main__':
 
   args = parseArguments()
   textinput = None
+  infname = None
   with args.infile as f:
     textinput = [line.strip() for line in f]
+    infname = f.name
+    if args.nlast:
+      textinput = textinput[-args.nlast:]
 
   timestamps, levels = readInput(textinput)
 
@@ -57,12 +61,12 @@ if __name__ == '__main__':
   ax.plot(x, levels)
   if args.threshold:
     ax.plot(x, len(x)*[args.threshold], '--')
-  #ax.set_ylim(-0.4, 0)
   ax.set_xlim(0, len(levels)-1)
   xticks = ax.get_xticks()
   xticks[-1] = len(levels)-1
   ax.set_xticks(xticks)
   ax.set_xticklabels([timestamps[int(x)] for x in xticks], rotation = 45, ha="right")
   ax.set_ylabel('Pegelstand')
+  ax.set_title(os.path.splitext(infname)[0])
   plt.savefig(args.outfile, bbox_inches = 'tight')
 
